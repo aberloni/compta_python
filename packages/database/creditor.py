8e2ef0384fc5a5283ffb7083aa80241d0724c0e3
ab <andre.berlemont@gmail.com>
2024-02-06 19:03:55 +0100

    work on creditors

--- packages/database/creditor.py
+++ packages/database/creditor.py
@@ -13,7 +13,40 @@ class Creditor:
 
         files = getAllFilesInFolder(path)
 
-        print(files)
+        #print(files)
 
-        # foreach...
+        for file in files:
+            
+            # ie : Z:/path/to/cre/file/resto.cre
 
+            key = file[:-4] # remove ext
+
+            # remove path
+            key = key.replace("\\","/") 
+            key = key[key.rfind("/")+1:]
+            
+            self.filters[key] = self.solveContent(file)
+
+        print(self.filters)
+
+    def solveContent(self, file):
+
+        blob = {}
+
+        content = loadFile(file)
+        print(content)
+
+        list = []
+        for c in content:
+            values = []
+            if ":" in c:
+                values = c.split(":")
+            else:
+                values.append(c)
+            
+            list.append(values)
+
+        blob["content"] = list
+        blob["location"] = ""
+
+        return blob
